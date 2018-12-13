d97d871ac865bf00ab3dbe65a69d6f72facdca75
Alex Weisberger <alex.m.weisberger@gmail.com>
2018-12-12 20:46:14 -0500

    Initial commit

---
+++ WhatToDoApplication/WhatToDoApplication.h
@@ -0,0 +1,19 @@
+//
+//  WhatToDoApplication.h
+//  WhatToDoApplication
+//
+//  Created by Alex on 12/12/18.
+//  Copyright © 2018 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WhatToDoApplication.
+FOUNDATION_EXPORT double WhatToDoApplicationVersionNumber;
+
+//! Project version string for WhatToDoApplication.
+FOUNDATION_EXPORT const unsigned char WhatToDoApplicationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WhatToDoApplication/PublicHeader.h>
+
+
