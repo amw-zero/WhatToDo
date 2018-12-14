f70edf0b248ca6460861c8851ad6cac0321e23ac
Alex Weisberger <alex.m.weisberger@gmail.com>
2018-12-14 13:20:15 -0500

    Rename Application Layer -> Logic

---
+++ WhatToDoLogic/WhatToDoLogic.h
@@ -0,0 +1,19 @@
+//
+//  WhatToDoLogic.h
+//  WhatToDoLogic
+//
+//  Created by Alex on 12/14/18.
+//  Copyright © 2018 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WhatToDoLogic.
+FOUNDATION_EXPORT double WhatToDoLogicVersionNumber;
+
+//! Project version string for WhatToDoLogic.
+FOUNDATION_EXPORT const unsigned char WhatToDoLogicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WhatToDoLogic/PublicHeader.h>
+
+
---
+++ WhatToDoLogicTests/WhatToDoLogicTests.swift
@@ -0,0 +1,14 @@
+//
+//  WhatToDoLogicTests.swift
+//  WhatToDoLogicTests
+//
+//  Created by Alex on 12/14/18.
+//  Copyright © 2018 Alex. All rights reserved.
+//
+
+import XCTest
+@testable import WhatToDoLogic
+
+class WhatToDoLogicTests: XCTestCase {
+
+}
